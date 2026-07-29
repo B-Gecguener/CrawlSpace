ebb253277b89b2786e4454a264d6ad0353d0f33d
Marco <133157182+ProfessorClever@users.noreply.github.com>
2026-07-30 00:55:37 +0200

    create working level_selection

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,82 @@
-use std::collections::HashMap;
-use std::{fs, io};
+use std::{
+    fs,
+    io,
+    path::PathBuf, // Besser weil OS unabhänig und funktioniert als path
+};
 
 fn main() {
+    greet_user();
+
+    let level = level_selection();
+
+    println!("Selected level:");
+    println!("{}", level.display());
+}
+
+fn greet_user() {
     println!("Hello, Adventurer!");
     println!("Welcome to CrawlSpace.");
-
-    choose_level();
-    get_user_input();
 }
 
 fn get_user_input() -> String {
     let mut input = String::new();
 
-    println!("Enter something:");
-
     io::stdin()
         .read_line(&mut input)
-        .expect("Failed to read line");
+        .expect("Failed to read input");
 
-    // input includes the trailing newline, so trim it
-    let input = input.trim();
-    return input.to_string();
+    input.trim().to_owned() // Der Rust-"Style", das letzte Statement ohne ";" wird automatisch returned
 }
 
-fn get_levels() -> HashMap<std::ffi::OsString, fs::DirEntry> {
-    let entries = fs::read_dir("data").expect("Failed to read directory");
-    let mut levels = HashMap::new();
+fn get_levels() -> Vec<PathBuf> {
+    let mut levels = Vec::new();
+
+    let entries = fs::read_dir("data")
+        .expect("Could not open data directory");
+
     for entry in entries {
-        let entry = entry.expect("Failed to read entry");
-        let file_name = entry.file_name();
-        levels.insert(file_name, entry);
+        let entry = entry.expect("Invalid directory entry");
+
+        let path = entry.path();
+
+        if path.is_dir() {
+            levels.push(path);
+        }
     }
-    return levels;
+
+    levels
 }
 
-fn choose_level() {
-    let levels = get_levels();
+fn level_selection() -> PathBuf {
+    let levels: Vec<PathBuf> = get_levels();
 
-    print!("Choose your level: ");
-    let level_names = levels.keys();
-    for name in level_names {
-        println!("{}", name.to_string_lossy())
-    }
-    let input = get_user_input();
-    for name in level_names {
-        if (name == input) {}
+    loop { // Rust mag "while true" nicht, loop ist anscheinden der weg für endlos-loops
+        println!();
+        println!("Available levels:");
+
+        for level in &levels {
+            println!(
+                "- {}",
+                level.file_name().unwrap().to_string_lossy()
+            );
+        }
+
+        println!();
+        println!("Choose a level:");
+
+        let input: String = get_user_input();
+
+        for level in &levels {
+            let name = level
+                .file_name()
+                .unwrap()
+                .to_string_lossy();
+
+            if name == input {
+                return level.clone();
+            }
+        }
+
+        println!("Level '{}' does not exist.", input);
     }
 }
