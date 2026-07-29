f710fd7af6f8db3da7baad079a6c8ca17a4b6e14
Batu <batu@MacBook-Air-von-Batu.local>
2026-07-29 16:43:57 +0200

    first tryout

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,24 @@
-use std::{fs, io}
+use std::{fs, io};
 
 fn main() {
     println!("Hello, Adventurer!");
     println!("Welcome to CrawlSpace.");
+    println!("Choose your level:");
+    get_user_input();
+}
+
+fn get_user_input() -> String {
+    let mut input = String::new();
+
+    println!("Enter something:");
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    // input includes the trailing newline, so trim it
+    let input = input.trim();
+
+    println!("You entered: {}", input);
+    return input.to_string();
 }
