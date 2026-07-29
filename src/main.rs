6795c29b5d0e60b99a409091f81bf98b16aa46df
Batu <batu@Air-von-Batu.fritz.box>
2026-07-29 23:54:28 +0200

    .

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+use std::collections::HashMap;
 use std::{fs, io};
 
 fn main() {
     println!("Hello, Adventurer!");
     println!("Welcome to CrawlSpace.");
-    println!("Choose your level:");
+
+    choose_level();
     get_user_input();
 }
 
@@ -18,7 +20,30 @@ fn get_user_input() -> String {
 
     // input includes the trailing newline, so trim it
     let input = input.trim();
-
-    println!("You entered: {}", input);
     return input.to_string();
+}
+
+fn get_levels() -> HashMap<std::ffi::OsString, fs::DirEntry> {
+    let entries = fs::read_dir("data").expect("Failed to read directory");
+    let mut levels = HashMap::new();
+    for entry in entries {
+        let entry = entry.expect("Failed to read entry");
+        let file_name = entry.file_name();
+        levels.insert(file_name, entry);
+    }
+    return levels;
+}
+
+fn choose_level() {
+    let levels = get_levels();
+
+    print!("Choose your level: ");
+    let level_names = levels.keys();
+    for name in level_names {
+        println!("{}", name.to_string_lossy())
+    }
+    let input = get_user_input();
+    for name in level_names {
+        if (name == input) {}
+    }
 }
